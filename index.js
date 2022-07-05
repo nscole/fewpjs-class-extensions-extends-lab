c4626fa9146a82f0067aa44ecfbb6228bfd8fc10
Naomi Cole <naomisarahcole@gmail.com>
2022-07-05 14:46:13 +1000

    Passing 6 tests

--- index.js
+++ index.js
@@ -16,3 +16,9 @@ class Polygon {
         }
     }
 };
+
+class Triangle extends Polygon {
+        get isValid() {
+            return (this.sides[0] + this.sides[1]) > this.sides[2] && (this.sides[1] + this.sides[2]) > this.sides[0];
+    }
+};
